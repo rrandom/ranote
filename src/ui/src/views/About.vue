b889c485fe4a5e0c66c8b06d3251c8c613527e45
rrandom <emanonhere@gmail.com>
2019-05-23 18:09:30 +0800

    quick markdown

--- src/ui/src/views/About.vue
+++ src/ui/src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
+  <div class="about" v-html="result">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Markdown from 'markdown-it';
+
+@Component({
+  beforeRouteEnter (to, from, next) {
+    let content = window.localStorage.getItem('content');
+    console.log(content);
+    let md = new Markdown();
+    let result = md.render(content);
+    console.log(result);
+    next(vm => {
+      vm.result = result;
+    });
+  }
+})
+export default class About extends Vue {
+
+  result = '';
+
+  mounted() {
+  }
+
+  
+}
+</script>
--- src/ui/src/views/Home.vue
+++ src/ui/src/views/Home.vue
@@ -5,29 +5,34 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-import codemirror from 'codemirror';
-// import 'codemirror/mode/javascript/javascript';
+import CodeMirror from 'codemirror';
+
 import 'codemirror/mode/markdown/markdown';
-
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/midnight.css';
 import 'codemirror/keymap/emacs';
 
 @Component({
-  components: {
-    HelloWorld,
+  beforeRouteLeave(to, from, next) {
+    let value =  this.cm.getDoc().getValue();
+    window.localStorage.setItem('content', value);
+    next();
   },
 })
 export default class Home extends Vue {
+
+  public cm: null | CodeMirror.Editor = null;
+
   public mounted() {
-    const k = codemirror(this.$el as HTMLElement, {
+    const cm = CodeMirror(this.$el as HTMLElement, {
       value: 'function myScript(){return 100;}\n',
       mode:  'markdown',
       theme: 'midnight',
       lineNumbers: true,
+      keyMap: 'emacs',
     });
+    this.cm = cm;
   }
 }
 </script>
