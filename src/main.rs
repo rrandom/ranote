ae67a82b8f71081add979d926e565216bb986229
rrandom <emanonhere@gmail.com>
2019-05-27 17:50:24 +0800

    create workspace dir in home dir

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-extern crate web_view;
 
-use web_view::*;
+use std::{fs};
 
 fn main() {
+
+    let path = dirs::home_dir().and_then(|mut h| {
+        h.push(".ranote");
+        Some(h.into_os_string())
+    }).unwrap();
+
+    dbg!(&path);
+
+    if let Err(_) = fs::metadata(&path) {
+        fs::create_dir(&path).unwrap();
+    }
+
+    let files: Vec<_> = fs::read_dir(path).unwrap().collect();
+
+    dbg!(&files);
+
     web_view::builder()
-        .title("Juggernaut")
+        .title("Ranote")
         // .content(Content::Html(include_str!("web/index.html")))
-        .content(Content::Url("http://localhost:8080"))
+        .content(web_view::Content::Url("http://localhost:8080"))
         .size(800, 600)
         .resizable(true)
         .debug(true)
