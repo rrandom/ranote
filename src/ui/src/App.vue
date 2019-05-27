1aa86246ebb37f50cd183e64633016bd9b492a51
rrandom <emanonhere@gmail.com>
2019-05-27 16:58:54 +0800

    mock bars

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Editor</router-link> |
-      <router-link to="/preview">Preview</router-link>
+    <LeftBar />
+    <MiddleBar />
+    <div class="editor-panel">
+      <div id="nav">
+        <router-link to="/">Editor</router-link> |
+        <router-link to="/preview">Preview</router-link>
+      </div>
+      <keep-alive>
+        <router-view/>
+      </keep-alive>
     </div>
-    <keep-alive>
-      <router-view/>
-    </keep-alive>
   </div>
 </template>
+<script>
+import LeftBar from '@/components/LeftBar';
+import MiddleBar from '@/components/MiddleBar';
 
+export default {
+  components: {
+    LeftBar,
+    MiddleBar,
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+
+  display: flex;
+  flex-direction: row;
 }
+
+
 #nav {
   padding: 30px;
   a {
---
+++ src/ui/src/components/LeftBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    left bar
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
