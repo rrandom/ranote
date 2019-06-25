10e5aed08a2b39e398b3c5393fbccf9318829f39
rrandom <emanonhere@gmail.com>
2019-06-25 15:51:15 +0800

    doc.rs minor update

--- core/src/doc.rs
+++ core/src/doc.rs
@@ -2,20 +2,11 @@ use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 use std::path::PathBuf;
+use crate::error::Result;
 
 pub struct Doc {
     path: PathBuf,
     reader: BufReader<File>,
     writer: BufWriter<File>,
     tags: BTreeSet<String>,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn name() {
-        unimplemented!();
-    }
 }
---
+++ core/tests/doc.rs
@@ -0,0 +1,6 @@
+use ranote_core::{doc::Doc};
+
+#[test]
+fn cli_no_args() {
+    println!("{}", "abcd");
+}
