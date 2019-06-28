dcc56d1c6bd22d35842019b63c07db26f5405c3c
rrandom <emanonhere@gmail.com>
2019-06-28 09:41:22 +0800

    fix test

--- core/tests/doc.rs
+++ core/tests/doc.rs
@@ -1,14 +1,14 @@
-use ranote_core::{doc::Doc, error::Result};
+use ranote_core::{note::Note, error::Result};
 use tempfile::TempDir;
 
 #[test]
 fn create_new_doc() -> Result<()> {
     let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    let doc = Doc::new(temp_dir.path(), "tmp")?;
+    let doc = Note::new(temp_dir.path(), "tmp")?;
 
     drop(doc);
 
-    let mut doc = Doc::open(temp_dir.path().join("tmp"))?;
+    let mut doc = Note::open(temp_dir.path().join("tmp"))?;
     assert_eq!(doc.content, "");
 
     doc.write("test content".to_owned())?;
